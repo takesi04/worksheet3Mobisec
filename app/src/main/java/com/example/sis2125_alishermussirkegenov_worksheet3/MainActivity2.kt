a79eddf90564dba3ea412c8adcf5c09bee07a03d
takesi04 <alish.muratov.99@bk.ru>
2024-02-28 15:26:50 +0600

    worksheet3

---
+++ app/src/main/java/com/example/sis2125_alishermussirkegenov_worksheet3/MainActivity2.kt
@@ -0,0 +1,11 @@
+package com.example.sis2125_alishermussirkegenov_worksheet3
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity2 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main2)
+    }
+}
